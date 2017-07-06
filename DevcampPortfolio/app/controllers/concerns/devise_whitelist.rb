4180b6e2676083f7f14684a7b64d733c35164fb1
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 17:52:18 +0000

    files that don't work when logging in and signing out

--- DevcampPortfolio/app/controllers/concerns/devise_whitelist.rb
+++ DevcampPortfolio/app/controllers/concerns/devise_whitelist.rb
@@ -2,7 +2,8 @@ module DeviseWhitelist
   extend ActiveSupport::Concern
   
   included do 
-    before_filter :configure_permitted_parameters, if: :devise_controller?
+   #before_filter :configure_permitted_parameters, if: :devise_controller?
+    skip_before_filter :verify_authenticity_token
   end
   
   def configure_permitted_parameters
