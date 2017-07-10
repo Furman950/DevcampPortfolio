66bf9cd39384f8f49da919ef679606feb69e4b81
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 15:05:40 +0000

    Implemented view helper for auth links

--- DevcampPortfolio/app/controllers/concerns/devise_whitelist.rb
+++ DevcampPortfolio/app/controllers/concerns/devise_whitelist.rb
@@ -2,8 +2,7 @@ module DeviseWhitelist
   extend ActiveSupport::Concern
   
   included do 
-   #before_filter :configure_permitted_parameters, if: :devise_controller?
-    skip_before_filter :verify_authenticity_token
+   before_filter :configure_permitted_parameters, if: :devise_controller?
   end
   
   def configure_permitted_parameters
