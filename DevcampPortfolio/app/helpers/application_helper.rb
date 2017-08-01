38c134261dadfd7abf992131706190fa13f38675
Fermin Sandoval <ferminsandoval950@hotmail.com>
2017-08-01 14:23:16 +0000

    Refactored alerts into help method

--- DevcampPortfolio/app/helpers/application_helper.rb
+++ DevcampPortfolio/app/helpers/application_helper.rb
@@ -57,4 +57,17 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+  
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    
+    if alert
+      js add_gritter(alert, title: 'Fermin Sandoval', sticky: false)
+    end
+  end
+  
+  def alert_generator msg
+    js add_gritter(msg, title: "Fermin Sandoval", sticky: false)
+  end
+  
 end
