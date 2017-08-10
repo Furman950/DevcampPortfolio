8a33450b40e24de72f4184d3e923475203ececea
Fermin Sandoval <ferminsandoval950@hotmail.com>
2017-08-10 16:22:32 +0000

    Added validations for topics in blogs and added a topic form element

--- DevcampPortfolio/app/controllers/blogs_controller.rb
+++ DevcampPortfolio/app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
@@ -9,9 +9,8 @@ class BlogsController < ApplicationController
     if logged_in?(:site_admin)
       @blogs = Blog.recent.page(params[:page]).per(5)
     else
-      @blogs = Blog.published.page(params[:page]).per(5)
+      @blogs = Blog.published.recent.page(params[:page]).per(5)
     end
-    
     @page_title = "My Portfolio Blog"
   end
 
@@ -21,11 +20,11 @@ class BlogsController < ApplicationController
     if logged_in?(:site_admin) || @blog.published?
       @blog = Blog.includes(:comments).friendly.find(params[:id])
       @comment = Comment.new
-      
+
       @page_title = @blog.title
       @seo_keywords = @blog.body
     else
-      redirect_to blogs_path, notice: "You are not authorized to acces this page"
+      redirect_to blogs_path, notice: "You are not authorized to access this page"
     end
   end
 
@@ -92,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
